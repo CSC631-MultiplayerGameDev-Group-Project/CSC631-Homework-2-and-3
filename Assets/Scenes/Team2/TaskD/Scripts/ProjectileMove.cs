ca7d39a40a55cd090359e34b7fab2fb005932927
ChristianXT <ctoscano@mail.sfsu.edu>
2023-03-01 19:57:45 -0800

    Task D
    Particle effect created.
    3 scripts created to control particle.
    ProjectileMove allows the particle to move.
    RotateToMouse control the sphere firepoint allowing you to aim.
    SpawnProjectile spawns the projectile when clicking on the mouse button.

---
+++ Assets/Scenes/Team2/TaskD/Scripts/ProjectileMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public float firRate;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speed != 0){
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }else{
+            Debug.Log("no Speed!");
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) { 
+        speed = 0;
+
+        Destroy(gameObject);
+    }
+}
+
